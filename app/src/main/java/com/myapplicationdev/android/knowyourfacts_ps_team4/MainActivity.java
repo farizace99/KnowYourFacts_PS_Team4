cafd9fc8d742a298a4b30bd06639a07f4360121c
ooishujing <18016012@myrp.edu.sg>
2021-05-27 13:56:30 +0800

    main

--- app/src/main/java/com/myapplicationdev/android/knowyourfacts_ps_team4/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/knowyourfacts_ps_team4/MainActivity.java
@@ -1,14 +1,79 @@
 package com.myapplicationdev.android.knowyourfacts_ps_team4;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ArrayList<Fragment> al;
+    MyFragmentPagerAdapter adapter;
+    ViewPager vPager;
+    Button btnRead;
+    MenuItem item;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnRead = findViewById(R.id.btnRead);
+        vPager = findViewById(R.id.vp);
+
+        FragmentManager fm = getSupportFragmentManager();
+
+        al = new ArrayList<Fragment>();
+        al.add(new Fragment_Frag1());
+        al.add(new Fragment_Frag2());
+        al.add(new Fragment_Frag3());
+
+        adapter = new MyFragmentPagerAdapter(fm, al);
+
+        vPager.setAdapter(adapter);
+
+        @Override
+        public boolean onOptionsItemSelected (MenuItem item){
+            int id = item.getItemId();
+            switch (id) {
+                case R.id.action_previous:
+                    if (vPager.getCurrentItem() > 0) {
+                        int previousPage = vPager.getCurrentItem() - 1;
+                        vPager.setCurrentItem(previousPage, true);
+                    }
+                    break;
+                case R.id.action_random:
+                    // What to do here?
+                    break;
+                case R.id.action_next:
+                    int max = vPager.getChildCount();
+                    if (vPager.getCurrentItem() < max - 1) {
+                        int nextPage = vPager.getCurrentItem() + 1;
+                        vPager.setCurrentItem(nextPage, true);
+                        break;
+                    }
+                    return super.onOptionsItemSelected(item);
+            }
+
+
+            btnRead.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                }
+
+            });
+        }
     }
-}
