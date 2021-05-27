dc165c6ed14f7357c567e2eea20f636dd671ed9b
18050674 <18050674@myrp.edu.sg>
2021-05-27 11:53:03 +0800

    Initial commit

---
+++ app/src/main/java/com/myapplicationdev/android/knowyourfacts_ps_team4/Fragment_Frag_3.java
@@ -0,0 +1,14 @@
+package com.myapplicationdev.android.knowyourfacts_ps_team4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Fragment_Frag_3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment__frag_3);
+    }
+}
---
+++ app/src/main/java/com/myapplicationdev/android/knowyourfacts_ps_team4/ScheduledNotificationReceiver.java
@@ -0,0 +1,4 @@
+package com.myapplicationdev.android.knowyourfacts_ps_team4;
+
+public class ScheduledNotificationReceiver {
+}
